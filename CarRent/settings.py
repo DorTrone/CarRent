8f60a7b207b6ca5a8c163dfcae770059f12ea7b3
Dortrone <86688941+DorTrone@users.noreply.github.com>
2025-09-06 13:45:16 +0500

    new

--- CarRent/settings.py
+++ CarRent/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'CarRent.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://...')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
